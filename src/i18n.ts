2344be0303c6158a375964d5bf0ce371cad3d0ce
Katsuya Hidaka <hidakatsuya@gmail.com>
2022-09-23 23:18:37 +0900

    bump vue-i18n and fix type error by simplifying locale file loading

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,20 +1,9 @@
-import { createI18n, LocaleMessages, VueMessageType } from 'vue-i18n';
-
-function loadLocaleMessages () {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
-  const messages: LocaleMessages<VueMessageType> = {};
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
-    if (matched && matched.length > 1) {
-      const locale = matched[1];
-      messages[locale] = locales(key);
-    }
-  });
-  return messages;
-}
+import { createI18n } from 'vue-i18n';
+import en from './locales/en.json';
+import ja from './locales/ja.json';
 
 export default createI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: loadLocaleMessages()
+  messages: { en, ja }
 });
