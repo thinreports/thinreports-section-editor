33a1ecd69092c6b2c508497a971c26641511db7b
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-01-17 01:01:07 +0900

    Fixed error: Not available in legacy mode

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,5 +5,6 @@ import ja from './locales/ja.json';
 export default createI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: { en, ja }
+  messages: { en, ja },
+  legacy: false
 });
